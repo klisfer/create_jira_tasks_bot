4af99979a6f481ebe773efbdca4e810f0575b10c
aayush chaubey <aayush.chaubey674@gmail.com>
2019-04-09 16:29:17 +0530

    fixed issue , posting image as attachment

--- app/index.js
+++ app/index.js
@@ -1,27 +1,36 @@
 const dotenv = require("dotenv");
-const Bot = (require("@dlghq/dialog-bot-sdk"));
-const { MessageAttachment, ActionGroup, Action, Button } = (require("@dlghq/dialog-bot-sdk"));
-const { flatMap } = require('rxjs/operators');
-const axios = require('axios');
-const { merge } = require('rxjs');
-const moment = require('moment');
-
+const Bot = require("@dlghq/dialog-bot-sdk");
+const {
+  MessageAttachment,
+  ActionGroup,
+  Action,
+  Button
+} = require("@dlghq/dialog-bot-sdk");
+const { flatMap } = require("rxjs/operators");
+const axios = require("axios");
+const { merge } = require("rxjs");
+const moment = require("moment");
+const fs = require("fs");
+const request = require("request");
+var util  = require('util');
+var fetch = require('isomorphic-fetch');
 
 dotenv.config();
 
-
-const credentials = process.env.JIRA_USERNAME + ":" + process.env.JIRA_API_TOKEN
-const credsBase64 = Buffer.from(credentials).toString('base64');
+const credentials =
+  process.env.JIRA_USERNAME + ":" + process.env.JIRA_API_TOKEN;
+const credsBase64 = Buffer.from(credentials).toString("base64");
 var fetchedProjects = [];
+var addedIssueKey = "";
 var jiraTaskTitle = "";
 var messageToReturn = {
-   peer: "",
-   id: "",
-   text: ""
+  peer: "",
+  id: "",
+  text: ""
 };
 const headers = {
-  'Authorization': 'Basic ' + credsBase64,
-  'Content-Type': 'application/json',
+  Authorization: "Basic " + credsBase64,
+  "Content-Type": "application/json"
 };
 
 async function run(token, endpoint) {
@@ -30,11 +39,10 @@ async function run(token, endpoint) {
     endpoints: [endpoint]
   });
 
-  //fetching bot name  
+  //fetching bot name
   const self = await bot.getSelf();
   console.log(`I've started, post me something @${self.nick}`);
-
-
+  console.log(credentials,credsBase64);
 
   bot.updateSubject.subscribe({
     next(update) {
@@ -42,55 +50,43 @@ async function run(token, endpoint) {
     }
   });
 
-
-  //subscribing to incoming messages 
-  const messagesHandle = bot
-    .subscribeToMessages()
-    . pipe(flatMap(async (message) => {
-      
+  //subscribing to incoming messages
+  const messagesHandle = bot.subscribeToMessages().pipe(
+    flatMap(async message => {
       // console.log("YAYAY", message);
 
       messageToReturn.peer = message.peer;
       messageToReturn.id = message.id;
-      if (message.content.type === 'text') {
-        
+      if (message.content.type === "text" && message.attachment === null) {
         jiraTaskTitle = await message.content.text;
-        
+
         const projects = await axios({
           url: process.env.JIRA_URL,
-          method: 'get',
-          headers: headers,
+          method: "get",
+          headers: headers
+        });
 
-        })
-
-        projects.data.map(project => {
+        projects.data.values.map(project => {
           fetchedProjects.push(project);
-        })
-
-
+        });
 
         //creating available project options
         const dropdownActions = [];
-        fetchedProjects.map((project) => {
+        fetchedProjects.map(project => {
           const action = Action.create({
             id: project.name,
             widget: Button.create({ label: project.name })
-          })   
+          });
           dropdownActions.push(action);
-
         });
 
-        
-        
         //adding stop button to the actions
         const stopAction = Action.create({
-          id: 'stop',
-          widget: Button.create({ label: 'stop' })
-        })
+          id: "stop",
+          widget: Button.create({ label: "stop" })
+        });
         dropdownActions.push(stopAction);
 
-        
-        
         // returning the projects to the messnger
         const mid = await bot.sendText(
           message.peer,
@@ -98,114 +94,168 @@ async function run(token, endpoint) {
           MessageAttachment.reply(message.id),
           ActionGroup.create({
             actions: dropdownActions
-          }),
+          })
         );
-      }
-    }));
+      } else if (message.attachment.type === "reply") {
+        console.log("message", message);
+        if (message.content.type === "text") {
+          const commentUrl =
+            process.env.JIRA_ISSUE_CREATE + "/" + addedIssueKey + "/comment";
+          var bodyData = {
+            body: message.content.text
+          };
+          const postIssueToJira = await axios({
+            url: commentUrl,
+            method: "post",
+            headers: headers,
+            data: bodyData
+          });
+          console.log("POSTED", postIssueToJira);
+        } else if (message.content.type === "document") {
+          const attachmentUrl =
+            process.env.JIRA_ISSUE_CREATE_C +
+            "/" +
+            addedIssueKey +
+            "/attachments";
+          const loc = process.env.FILE_UPLOAD_PATH + message.content.name;
+         
+          const imgUrl = process.env.FILE_UPLOAD_PATH + message.content.name; 
+          var options = { method: 'POST',
+          url: attachmentUrl,
+          headers: 
+           { 
+             Authorization: "Basic " + credsBase64,
+             'cache-control': 'no-cache,no-cache',
+             'X-Atlassian-Token': 'no-check',
+             'content-type': 'multipart/form-data; boundary=----WebKitFormBoundary7MA4YWxkTrZu0gW' },
+          formData: 
+           { file: 
+              { value: fs.createReadStream(imgUrl),
+                options: 
+                 { filename: imgUrl,
+                   contentType: null } } } };
 
+            
+            
+    
+         
+           const postIssueToJira = await request(options, function (error, response, body) {
+            if (error) throw new Error(error);
+          
+            console.log("OUTPUT",body);
+
+          });           
+
+          console.log("RESPONSE",postIssueToJira);
+        }
+      }
+    })
+  );
 
   //creating action handle
-  const actionsHandle = bot
-    .subscribeToActions()
-    .pipe(flatMap(async (event) =>{
-     if(event.id !== 'stop'){
+  const actionsHandle = bot.subscribeToActions().pipe(
+    flatMap(async event => {
+      if (event.id !== "stop") {
+        const projectToPost = await fetchedProjects.filter(
+          project => project.name === event.id
+        );
+        console.log("POST", projectToPost);
+        const dataToPost = {
+          fields: {
+            project: {
+              key: projectToPost[0].key
+            },
+            summary: jiraTaskTitle,
+            description:
+              "Creating of an issue using project keys and issue type names using the REST API",
+            issuetype: {
+              name: "Task"
+            }
+          }
+        };
+
+        //creating the issue in JIRA
+        const postIssueToJira = await axios({
+          url: process.env.JIRA_ISSUE_CREATE,
+          method: "post",
+          headers: headers,
+          data: dataToPost
+        });
+        
       
-      const projectToPost = await fetchedProjects.filter(project => (project.name === event.id));
-      const dataToPost ={ "fields": {
-        "project":
-        {
-           "key": projectToPost[0].key
-        },
-        "summary": jiraTaskTitle,
-        "description": "Creating of an issue using project keys and issue type names using the REST API",
-        "issuetype": {
-           "name": "Task"
-        }
-    }} 
-     
-    
-    //creating the issue in JIRA
-    const postIssueToJira = await axios({
-        url: process.env.JIRA_ISSUE_CREATE,
-        method: 'post',
-        headers: headers,
-        data: dataToPost
-      });     
-      
+        // return the response to messenger
+        const responseText = formatJiraText(
+          postIssueToJira.data,
+          projectToPost[0],
+          jiraTaskTitle
+        );
+        messageToReturn.text = responseText;
+        const mid = await sendTextToBot(bot, messageToReturn);
+        //set the returned issue to addedIssueKey
+        addedIssueKey = postIssueToJira.data.id;
+      } else {
+        //code for when stop button is clicked
+        messageToReturn.text = "Task addition cancelled by user";
+        const mid = await sendTextToBot(bot, messageToReturn);
+        fetchedProjects = [];
+        messageToReturn = {
+          id: "",
+          peer: "",
+          text: ""
+        };
+        jiraTaskTitle = "";
+      }
 
-    // return the response to messenger  
-      const responseText = formatJiraText(postIssueToJira.data , projectToPost[0] , jiraTaskTitle );
-      messageToReturn.text = responseText
-      const mid = await sendTextToBot(bot , messageToReturn)
-      
-   
-    }else{
-       //code for when stop button is clicked
-        messageToReturn.text = "Task addition cancelled by user"
-        const mid = await sendTextToBot(bot , messageToReturn);      
-       fetchedProjects = [];
-       messageToReturn = {
-         id: "",
-         peer:"",
-         text:""
-       };
-       jiraTaskTitle = "";
-    }
-  
-  
-    //resetting the variables
-    fetchedProjects = [];
-    messageToReturn = {
-      id: "",
-      peer:"",
-      text:""
-    };
-    jiraTaskTitle = "";
-  
-  }));
+      //resetting the variables
+      fetchedProjects = [];
+      messageToReturn = {
+        id: "",
+        peer: "",
+        text: ""
+      };
+      jiraTaskTitle = "";
+    })
+  );
 
-
-
-// merging actionHandle with messageHandle
+  // merging actionHandle with messageHandle
   await new Promise((resolve, reject) => {
-    merge(messagesHandle, actionsHandle)
-      .subscribe({
-        error: reject,
-        complete: resolve
-      });
-  }); 
-  
+    merge(messagesHandle, actionsHandle).subscribe({
+      error: reject,
+      complete: resolve
+    });
+  });
 }
 
 //token to connect to the bot
 const token = process.env.BOT_TOKEN;
-if (typeof token !== 'string') {
-  throw new Error('BOT_TOKEN env variable not configured');
+if (typeof token !== "string") {
+  throw new Error("BOT_TOKEN env variable not configured");
 }
 
 //bot endpoint
-const endpoint = process.env.BOT_ENDPOINT || 'https://grpc-test.transmit.im:9443';
+const endpoint =
+  process.env.BOT_ENDPOINT || "https://grpc-test.transmit.im:9443";
 
 run(token, endpoint)
-.then(response => console.log("RESPONDED", response))
-  .catch((error) => {
+  .then(response => console.log("RESPONDED", response))
+  .catch(error => {
     console.error(error);
     process.exit(1);
   });
 
-
-
-function formatJiraText(task , project , jiraTaskTitle) {
-  const outputFormat = "[" + project.key + "-" + task.id + "](" + task.self + ") :" + jiraTaskTitle;
+function formatJiraText(task, project, jiraTaskTitle) {
+  const outputFormat =
+    "[" +
+    project.key +
+    "-" +
+    task.id +
+    "](" +
+    task.self +
+    ") :" +
+    jiraTaskTitle;
   return outputFormat;
 }
 
 function sendTextToBot(bot, message) {
-  bot
-  .sendText(
-    message.peer,
-    message.text,
-    MessageAttachment.reply(message.id),
-  );
-
+  bot.sendText(message.peer, message.text, MessageAttachment.reply(message.id));
 }
